71a7833b067e37778565dfa64b64b8f4b949b0c7
ahmedslove3 <ahmed_slove3@hotmail.com>
2015-06-17 17:00:54 +0200

    v 0.1.2
    -fixed column type error in database
    -attached the right sql file

---
+++ mcintern.sql
@@ -0,0 +1,65 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 17, 2015 at 04:58 PM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `mcintern`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admins`
+--
+
+CREATE TABLE IF NOT EXISTS `admins` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(70) COLLATE utf8_bin NOT NULL,
+  `passward` text COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `people`
+--
+
+CREATE TABLE IF NOT EXISTS `people` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(70) CHARACTER SET utf8 NOT NULL,
+  `email` varchar(100) COLLATE utf8_bin NOT NULL,
+  `age` varchar(30) COLLATE utf8_bin NOT NULL,
+  `currentStudy` varchar(200) CHARACTER SET utf8 NOT NULL,
+  `university` varchar(70) CHARACTER SET utf8 NOT NULL,
+  `job` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `cv` varchar(100) COLLATE utf8_bin NOT NULL,
+  `comment` text COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `people`
+--
+
+INSERT INTO `people` (`id`, `name`, `email`, `age`, `currentStudy`, `university`, `job`, `cv`, `comment`) VALUES
+(1, 'sad', 'a', '17', 'master', 'sdf', 'back end', 'C:/wamp/www/hire-form/cvs/s.pdf', 'sdf');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
