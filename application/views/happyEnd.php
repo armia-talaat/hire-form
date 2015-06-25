93c18b19122898a1b449f48449e34cfc8fae658e
ahmedslove3 <ahmed_slove3@hotmail.com>
2015-06-25 17:27:05 +0200

    v 1.1.0
    -fixed broken css js links

--- application/views/happyEnd.php
+++ application/views/happyEnd.php
@@ -1,1 +1,62 @@
-Horray!!!
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1,minimum-scale=0.5">
+
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <!-- Place favicon.ico in the root directory -->
+        <!-- Bootstrap 3 -->
+        <script src="../../js/jquery-1.11.2.min.js"></script>
+        <script src="../../js/jquery-ui.js"></script>
+        <script src="../../js/jquery-scrollto.js"></script>
+        <link href="../../css/jquery-ui.css" rel="stylesheet">
+        <link href="../../css/bootstrap.css" rel="stylesheet">
+        <link href="../../css/bootstrap-theme.css" rel="stylesheet">
+        <script src="../../js/bootstrap.js"></script>
+        <!-- end of Bootstrap 3 -->
+        <link rel="stylesheet" href="../../css/normalize.css">
+        <link rel="stylesheet" href="../../css/main.css">
+        <script src="../../js/vendor/modernizr-2.8.3.min.js"></script>
+       
+    </head>
+    <!-- end of head -->
+    <!-- start of body -->
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+    <!-- start of inner -->
+
+
+
+     <div class="container-fluid">
+		<div class="row"><img src="img/banner.jpg" alt="Banner Image" class="img-responsive unselectable" draggable="false"></div>
+     </div><!-- end of container-fluid -->
+
+      <div class="container-fluid" id="form-section">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8 col-lg-offset-2">
+					<h1>Your application was submitted successfully<br> Thank you</h1>
+				</div>
+			</div>
+		</div>
+     </div><!-- end of container-fluid -->
+    
+     <!-- end of inner -->
+        <script>window.jQuery || document.write('<script src="js/jquery-1.11.2.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+        <script type="text/javascript">
+		window.onload=function(){
+		populatedropdown("daydropdown", "monthdropdown", "yeardropdown")
+		}
+		</script>
+    </body>
+    <!-- end of body -->
+</html>
